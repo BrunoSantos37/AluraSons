337475d7fd6f39aa1728a5ef143962d58e0137b4
Bruno Candido <Bc1620143@gmail.com>
2023-09-07 23:53:56 -0300

    Adicionando Javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const sons = document.querySelectorAll('.sons');
+const pom = document.querySelector('.som_tecla_pom');
+
+function tocaSom() {
+    pom.play();
+}
+
+//9 botões
+const teclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador <= teclas.length) {
+    teclas[contador].onclik = tocaSom;
+}

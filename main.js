e912b1f2709e37b48e8ef3667537026a9547187b
Bruno Candido <Bc1620143@gmail.com>
2023-09-10 00:31:38 -0300

    Funcionando!

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-
-const sons = document.querySelectorAll('.sons');
-const pom = document.querySelector('.som_tecla_pom');
-
-function tocaSom() {
-    pom.play();
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    
+    if (elemento && elemento.localName == 'audio') {
+        elemento.play();
+    }
+    else{
+        alert('Elemento não compatível!')
+    }
 }
 
 //9 botões
 const teclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for(i = 0; i < teclas.length; i++){
 
-while(contador <= teclas.length) {
-    teclas[contador].onclik = tocaSom;
-    contador++
+    const tecla = teclas[i]; // 1 tecla/botão 
+    const instrumento = tecla.classList[1]; //a 2° classe da tecla
+    const idSom = `#som_${instrumento}`;// id dos áudios com template string
+
+    tecla.onclick = function() {
+        tocaSom(idSom);
+    }
+    
+    tecla.onkeydown = function(evento){
+        console.log(evento.code)
+        if (evento.key == 'Space' || evento.key == 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 }
